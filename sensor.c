bc9acc06a899c7dc626fcc856d17d58c41ec6ed2
MaciejKaca <maciek.kaca@gmail.com>
2019-04-25 20:58:10 +0200

    Added timeout on measurment

--- sensor.c
+++ sensor.c
@@ -3,6 +3,11 @@
 const int triggerPin = 2;
 const int firstEchoPin = 3;
 const int lastEchoPin = (firstEchoPin+NUMBER_OF_SENSORS)-1;
+const int delayBetweenMeasurments = 10;
+const int triggerDuration = 10;
+const float soundSpeed = 340.3; //m/s
+const int sensorAccuracy = 2; //2-200cm
+const int sensorTimeout = ((float(sensorAccuracy)/soundSpeed)*2)*1000000;
 
 void setup() 
 {
@@ -16,19 +21,18 @@ void setup()
 void trigger()
 {
     digitalWrite(triggerPin, HIGH);
-    delayMicroseconds(10);
+    delayMicroseconds(triggerDuration);
     digitalWrite(triggerPin, LOW);
 }
 
 typedef struct SensorData
 {
     int sensorNumber :3,
-         measurment  :5;
+          measurment :10;
 } SensorData;
 
 void loop() 
 {
-    int sensorNumber;
     int duration;
     SensorData sensorData;
     memset(&sensorData, 0, sizeof(sensorData));
@@ -37,13 +41,11 @@ void loop()
     {
         trigger();
 
-        duration = pulseIn(echoPin, HIGH);
+        duration = pulseIn(echoPin, HIGH, sensorTimeout);
         sensorData.measurment = int((duration / 2) / 29.1f);
-        
+        sensorData.sensorNumber = echoPin - firstEchoPin;
         Serial.write((uint8_t *)&sensorData, sizeof(sensorData));
 
-        sensorNumber = echoPin - firstEchoPin;
-
-        delay(100);
+        delay(delayBetweenMeasurments);
     }
 }
